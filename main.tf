feef36e2d78e5a1423fdc61f4e3ea2e2b1e5776d
aduabgy <aduabgy@yahoo.com>
2023-01-29 22:27:22 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.REGION
+  #   access_key = ""
+  #   secret_key = ""	
+}
+
+resource "aws_instance" "intro" {
+  ami                    = var.AMI
+  instance_type          = var.TYPE
+  availability_zone      = var.ZONE
+  key_name               = "dove-key"
+  vpc_security_group_ids = ["sg-0b4f8ac8b6f5132b3"]
+  tags = {
+    Name    = "Dove-Instance"
+    Project = "Dove"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable REGION {
+  default = "eu-west-2"
+}
+
+variable TYPE {
+  default = "t2.micro"
+}
+variable ZONE {
+  default = "eu-west-2a"
+}
+
+variable AMI {
+  default = "ami-0d09654d0a20d3ae2"
+}
+
